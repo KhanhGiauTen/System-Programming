d1c1f9a0e414835a4ae8e76a25e4d0e380592426
KhanhGiauTen <nqkhanh2925@gmail.com>
2026-02-04 08:34:27 +0700

    add Q_01

--- Lab/Lab 4/Question_01/Program.cs
+++ Lab/Lab 4/Question_01/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace Question_01
 {
+    class Playerclass
+    {
+        public int Score;
+    }
+    struct PointStruct
+    {
+        public int X;
+        public int Y;
+    }
     internal class Program
     {
+        static Playerclass globalplayer = new Playerclass();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Q01: Stack vs Heap");
+            ProcessData();
+            Console.WriteLine("End of ProcessData. 'p1' is now out of scope");
+        }
+        static void ProcessData()
+        {
+            int localInt = 100;
+            PointStruct localPoint = new PointStruct { X = 1, Y = 2 };
+            Playerclass p1 = new Playerclass();
+            p1.Score = localInt;
+
+            Console.WriteLine($"p1 Score = {p1.Score}");
         }
     }
 }
